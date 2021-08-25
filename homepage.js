ee6234dd705442673eed1540c0bbf1b32c1261aa
imjord <dean.jay.goat@gmail.com>
2021-08-25 15:09:31 -0600

    search history not functioning correctly

--- homepage.js
+++ homepage.js
@@ -3,8 +3,11 @@
 var userFormEl = $("#user-form");
 var cityInputEl = $("#city-name")
 var myButton = $("button");
-
-
+var forTitle = $("#forecast-title");
+var forText = $(".forcast-text");
+var tempText = $(".temp")
+var searchHistory = [];
+var searchList = $("#search-history");
 
 
 
@@ -25,10 +28,38 @@ var key = "5eb7d8eaa2f23d433771db6d9ffdd905";
 
 $("button").click(function(event){
     event.preventDefault();
-    var myApiCall = `https://api.openweathermap.org/data/2.5/weather?q=${cityInputEl.val()}&appid=${key}`
+    var myApiCall = `https://api.openweathermap.org/data/2.5/forecast?q=${cityInputEl.val()}&appid=${key}`
     console.log(cityInputEl.val())
-    fetch(myApiCall).then(response => response.json()).then(data => console.log(data)).catch(err => alert("Wrong city name"));
+    forTitle.html(` Forcast for ${cityInputEl.val()}`)
+    searchHistory.push(cityInputEl.val())
+    fetch(myApiCall).then(response => response.json()).then(data => {
+        console.log(data)
+
+        tempValue = data["list"]['0']['main']['temp'];
+
+        tempText.html("Tempature " + tempValue);
+
+    }).catch(err => alert("Wrong city name"));
+
+    localStorage.setItem("search", JSON.stringify(searchHistory))
+    appendHistory();
+   
 })
+
+
+function appendHistory(){
+    localStorage.getItem("search", searchHistory)
+
+     for(i = 0; i < searchHistory.length; i++) {
+        
+        searchList.append("<li>"+ searchHistory[i]  + "</li>")
+        
+
+     }
+  
+   
+
+}
 
 
 
